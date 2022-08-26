70c78098caf2a69e9b87e947d03ae52d26564725
JOSHI Ankit <ankit.joshi@soprasteria.com>
2022-08-26 14:25:26 +0530

    comment change

--- LibraryManagementSystemProject/src/main/java/com/sopra/Library.java
+++ LibraryManagementSystemProject/src/main/java/com/sopra/Library.java
@@ -1,10 +1,13 @@
 package com.sopra;
 
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+//This is pojo class
 
 @Entity
 @Table(name="Library")
